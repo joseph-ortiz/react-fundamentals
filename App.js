c79667897eed88128cdab1f14609072c8df4bbd3
jortiz <josepho011235@gmail.com>
2016-04-13 13:56:27 -0400

    added properties

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Class component with state.
 class App extends React.Component{
     render(){
-        return <h1>Hello World</h1>
+        let txt = this.props.txt
+        return (
+            <div>
+            <h1>Hello World</h1>
+            <b>{txt}</b>
+            </div>
+        );
     }
 }
 
@@ -11,4 +18,16 @@ class App extends React.Component{
 ////statesless component
 //const App = () => <h1> Hello Eggheads</h1>
 
-export default App
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: 'this is the default txt'
+}
+
+ReactDOM.render(
+    <App cat={5}/>,
+    document.getElementById('app')
+);
