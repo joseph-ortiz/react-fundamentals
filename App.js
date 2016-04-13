66f3ba0295ca5b35aa23258be77a22c1614f294e
jortiz <josepho011235@gmail.com>
2016-04-13 13:59:57 -0400

    added basic state

--- App.js
+++ App.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 
 // Class component with state.
 class App extends React.Component{
+    constructor() {
+        super();
+        this.state = { txt: 'this is the state text'}
+    }
+    update(e) {
+      this.setState({txt: e.target.value})
+    }
     render(){
         let txt = this.props.txt
         return (
             <div>
+            <input type='text' onChange={this.update.bind(this)} />
             <h1>Hello World</h1>
-            <b>{txt}</b>
+            <b>{this.state.txt}</b>
             </div>
         );
     }
