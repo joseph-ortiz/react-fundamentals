b77376dc6b423c4cdbe3634c3712ee861611f028
jortiz <josepho011235@gmail.com>
2016-04-14 17:13:31 -0400

    added Slider with examples of accessing components using Refs

--- App.js
+++ App.js
@@ -5,20 +5,34 @@ import ReactDOM from 'react-dom';
 class App extends React.Component{
     constructor() {
         super();
-        this.state = { txt: 'this is the state text'}
+        this.state = {
+         red: 0,
+         green: 0,
+         blue: 0
+        }
         this.update = this.update.bind(this)
+
     }
     update(e) {
-      this.setState({txt: e.target.value})
+      this.setState({
+              red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+              blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value,
+              green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      })
     }
     render(){
         let txt = this.props.txt
         return (
             <div>
-            <Widget txt={this.state.txt} update={this.update}/>
-            <Widget txt={this.state.txt} update={this.update}/>
-            <Widget txt={this.state.txt} update={this.update}/>
-            <Widget txt={this.state.txt} update={this.update}/>
+                <Slider ref="red" update={this.update}/>
+                {this.state.red}
+                <br/>
+                <Slider ref="green" update={this.update}/>
+                {this.state.green}
+                <br/>
+                <Slider ref="blue" update={this.update}/>
+                {this.state.blue}
+                <br/>
             </div>
         );
     }
@@ -37,6 +51,19 @@ App.defaultProps = {
     txt: 'this is the default txt'
 }
 
+
+class Slider extends React.Component {
+  render(){
+    return (
+      <input ref="inp" type="range"
+        min="0"
+        max="255"
+        onChange={this.props.update}/>
+    );
+  }
+}
+/*
+ * Widget Component
 const Widget = (props) => {
     return (
         <div>
@@ -46,7 +73,7 @@ const Widget = (props) => {
         </div>
     )
 }
-
+*/
 ReactDOM.render(
     <App cat={5}/>,
     document.getElementById('app')
