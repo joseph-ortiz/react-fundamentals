5a40e9880e65f0fee83a0ad428dc0ee08ea6307d
jortiz <josepho011235@gmail.com>
2016-04-19 01:24:08 -0400

    hooked into mounting basics

--- App.js
+++ App.js
@@ -3,78 +3,64 @@ import ReactDOM from 'react-dom';
 
 // Class component with state.
 class App extends React.Component{
-    constructor() {
+    constructor(){
         super();
-        this.state = {
-         red: 0,
-         green: 0,
-         blue: 0
-        }
-        this.update = this.update.bind(this)
-
+        this.state = {val:0};
+        this.update= this.update.bind(this);
     }
-    update(e) {
-      this.setState({
-              red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-              blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value,
-              green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      })
+    update() {
+        this.setState({val: this.state.val +1 })
+    }
+    componentWillMount(){
+        console.log('mounting')
+    }
+    componentDidMount(){
+        console.log('mounted')
+    }
+    componentWillUnmount(){
+        console.log('bye')
     }
     render(){
-        let txt = this.props.txt
-        return (
-            <div>
-                <Slider ref="red" update={this.update}/>
-                {this.state.red}
-                <br/>
-                <Slider ref="green" update={this.update}/>
-                {this.state.green}
-                <br/>
-                <Slider ref="blue" update={this.update}/>
-                {this.state.blue}
-                <br/>
-            </div>
-        );
+        console.log('rendeirng!');
+        return <button onClick={this.update}>{this.state.val}</button>
     }
-}
-
-
-////statesless component
-//const App = () => <h1> Hello Eggheads</h1>
-
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-    txt: 'this is the default txt'
-}
-
-
-class Slider extends React.Component {
-  render(){
-    return (
-      <input ref="inp" type="range"
-        min="0"
-        max="255"
-        onChange={this.props.update}/>
+    }
+    class Wrapper extends React.Component {
+        constructor(){
+            super();
+        }
+        mount(){
+            ReactDOM.render(<App />, document.getElementById('a'))
+        }
+        unmount(){
+            ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+        }
+        render() {
+            return (
+                <div>
+                <button onClick={this.mount.bind(this)}> Mount</button>
+                <button onClick={this.unmount.bind(this)}> Unmount</button>
+                <div id="a"></div>
+                </div>
+            )
+        }
+    }
+    class Button extends React.Component {
+        render(){
+            return <button>{}</button>
+        }
+    }
+    class Slider extends React.Component {
+        render(){
+            return (
+                <input ref="inp" type="range"
+                min="0"
+                max="255"
+                onChange={this.props.update}/>
+            );
+        }
+    }
+    ReactDOM.render(
+        <Wrapper/>,
+        document.getElementById('app')
     );
-  }
-}
-/*
- * Widget Component
-const Widget = (props) => {
-    return (
-        <div>
-            <input type="text"
-            onChange={props.update}/>
-           <h1>{props.txt}</h1>
-        </div>
-    )
-}
-*/
-ReactDOM.render(
-    <App cat={5}/>,
-    document.getElementById('app')
-);
